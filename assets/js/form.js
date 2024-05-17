945e75ca977cc26e9aeadc30be5c7834ba918962
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-05-17 16:21:01 -0500

    added functionality that you cannot submit a blog post from the index.html if any of the fields are left blank

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,23 @@
+const userName = document.getElementById('userName');
+const blogTitle = document.getElementById('blogTitle');
+const blogContent = document.getElementById('blogContent');
+
+function saveEnteredBlogPost() {
+    // Save related form data as an object
+    const enteredBlogPost = {
+        userName: userName.value,
+        blogTitle: blogTitle.value,
+        blogContent: blogContent.value
+    };
+    localStorage.setItem('enteredBlogPost', JSON.stringify(enteredBlogPost));
+}
+
 submitButton.addEventListener('click', function () {
-    location.href = './blog.html';
+    if (userName.value === "" || blogTitle.value === "" || blogContent.value === "") {
+        alert('wrong submission')
+    }
+    else {
+        saveEnteredBlogPost();
+        location.href = './blog.html';
+    }
 });
