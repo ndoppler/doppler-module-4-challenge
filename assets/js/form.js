6194c6615ab03934c260ca025b48d529dc6326fe
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-05-18 18:28:32 -0500

    added the functionality that allows users to add multiple blog posts to the localstorage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,16 +1,28 @@
 const userName = document.getElementById('userName');
 const blogTitle = document.getElementById('blogTitle');
 const blogContent = document.getElementById('blogContent');
+let storedBlogPosts = [];
 
 function saveEnteredBlogPost() {
     // Save related form data as an object
-    const enteredBlogPost = {
+    let enteredBlogPost = {
         userName: userName.value,
         blogTitle: blogTitle.value,
         blogContent: blogContent.value
     };
-    localStorage.setItem('enteredBlogPost', JSON.stringify(enteredBlogPost));
-}
+
+    let storedBlogPosts = JSON.parse(localStorage.getItem('storedBlogPosts'));
+    console.log(storedBlogPosts)
+
+    if (storedBlogPosts === null) {
+        storedBlogPosts.push(enteredBlogPost);
+        localStorage.setItem('storedBlogPosts', JSON.stringify(storedBlogPosts));
+    }
+    else {
+        storedBlogPosts.push(enteredBlogPost);
+        localStorage.setItem('storedBlogPosts', JSON.stringify(storedBlogPosts));
+    }
+};
 
 submitButton.addEventListener('click', function () {
     if (userName.value === "" || blogTitle.value === "" || blogContent.value === "") {
